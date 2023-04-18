8a25d466a1abb796483aa2b24cce248d3fbbdfcc
mozta <perezaguirrerafael@gmail.com>
2023-04-18 12:59:42 -0600

    remove python-dotenv

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
